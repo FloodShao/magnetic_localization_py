0085ca9ef2de6294ace9391157643a96d7f7a73a
FloodShao <shaoguoliang1994@gmail.com>
2018-02-27 13:00:36 +0800

    0227 commit

--- PositionRetrieval.py
+++ PositionRetrieval.py
@@ -1,15 +1,15 @@
 import scipy.io as sio
 from lib.magnetPosition import magnetPos_from_setup
 from lib.LS_optimization import *
-import numpy as np
 from lib.PreProcessingParam import *
 from scipy.optimize import least_squares
+from scipy.optimize import minimize
 
 
 Bt = sio.loadmat("LabParam0222.mat")['Bt']
 sensorParam = sio.loadmat("LabParam0222.mat")['sensorParam']
 
-Idx = [0]
+Idx = [2]
 
 matdir = "./data/datamat0206/Geo_Mag.mat"
 data_cal = sio.loadmat(matdir)['data']
@@ -25,15 +25,71 @@ for i in Idx:
 #print(magnetPos)
 
 
-magnetPos_init = np.array([0., 0., 0., 0., 0.])
+magnetPos5d_init = np.array([0., 0., 0., 0., 0.])
+magnetPos6d_init = np.array([0., 0., 0., math.sqrt(1/3), math.sqrt(1/3), math.sqrt(1/3)])
+x_opt, y_opt, z_opt = 0., 0., 0.
 for i in range(data.shape[0]):
 
-    bounds = ([-190e-3, -120e-3, -140e-3, 0., 0.], [190e-3, 120e-3, 140e-3, math.pi, 2*math.pi])
+    '''least_squares'''
+    '''
+    bounds = ([-190e-3, -120e-3, -140e-3, 0., 0.], [190e-3, 120e-3, 140e-3, 180., 360.])
 
-    res = least_squares(MagnetPosError_5d, magnetPos_init, verbose=0, bounds=bounds, jac=MagnetPosError_5d_jac, ftol=1e-10, xtol=1e-8, method='trf',
-                        args=(Bt, sensorParam, data[i]))
+    res_5d = least_squares(MagnetPosError_5d, magnetPos5d_init, verbose=2, bounds=bounds,
+                           jac=MagnetPosError_5d_jac, ftol=1e-8, xtol=1e-6, method='trf',
+                           args=(Bt, sensorParam, data[i]))
+    print("lease_squares angle")
+    print(res_5d.x)
+    '''
 
-    print(res.x)
+    '''minimize_5d'''
+    '''
+    bounds_5d = ((-190e-3, 190e-3), (-120e-3, 120e-3), (-140e-3, 140e-3), (0., 180.), (0., 360.))
+    opt_5d = {'disp': False}
+    res = minimize(MagnetPosError_5d, magnetPos5d_init, args=(Bt, sensorParam, data[i]), method='L-BFGS-B',
+                   jac=MagnetPosError_5d_jac, bounds=bounds_5d, tol=1e-6, options=opt_5d)
+    #print('L-BFGS-B')
+    #print(res.x)
+    '''
+
+    '''minimize 6d'''
+
+    cons = {
+        'type':'eq',
+        'fun': MagnetPosError_cons,
+        'jac': MagnetPosError_cons_jac
+    }
+    bounds_6d = ((-190e-3, 190e-3), (-120e-3, 120e-3), (-140e-3, 140e-3), (-1., 1.), (-1., 1.), (-1., 1.))
+    opt = {'maxiter': 100, 'disp': False}
+    res_6d = minimize(MagnetPosError_6d, magnetPos6d_init, args=(Bt, sensorParam, data[i]), method='SLSQP',
+                   jac=MagnetPosError_6d_jac_orientation, bounds=bounds_6d, constraints=cons, tol=1e-6,
+                   options=opt)
+
+    m_opt, n_opt, p_opt = res_6d.x[3], res_6d.x[4], res_6d.x[5]
+
+    if math.isnan(m_opt) or math.isnan(n_opt) or math.isnan(p_opt):
+        print("[Warning] Failed to estimate the orientation of the magnet")
+        magnetPos6d_init = np.array([0., 0., 0., math.sqrt(1 / 3), math.sqrt(1 / 3), math.sqrt(1 / 3)])
+    else:
+        magnetPos6d_init = np.array([0., 0., 0., m_opt, n_opt, p_opt])
 
 
-print(p_m_original[Idx[0]])
+    #print(magnetPos6d_init)
+
+    bounds = ([-190e-3, -120e-3, -140e-3, -1., -1., -1.], [190e-3, 120e-3, 140e-3, 1., 1., 1.])
+    res_final = least_squares(MagnetPosError_6d, magnetPos6d_init, verbose=0, bounds = bounds,
+                           jac=MagnetPosError_6d_jac, ftol=1e-8, xtol=1e-10, method='trf',
+                           args=(Bt, sensorParam, data[i]))
+
+    x_opt, y_opt, z_opt = res_final.x[0], res_final.x[1], res_final.x[2]
+    print("optimization results")
+    print(res_final.x)
+
+    magnetPos6d_init = np.array([0., 0., 0., math.sqrt(1/3), math.sqrt(1/3), math.sqrt(1/3)])
+
+    print("original position")
+    print(magnetPos_from_setup(p_m_original[Idx[0]])[i])
+
+    print("\n")
+
+
+
--- lib/LS_optimization.py
+++ lib/LS_optimization.py
@@ -166,9 +166,6 @@ def MagnetPosError_5d(magnet_pos_5d, Bt, sensor_param, Measured_data):
 
 def MagnetPosError_5d_jac(magnet_pos_5d, Bt, sensor_param, Measured_data):
 
-    x = magnet_pos_5d[0]
-    y = magnet_pos_5d[1]
-    z = magnet_pos_5d[2]
     theta = magnet_pos_5d[3]
     phi = magnet_pos_5d[4]
 
@@ -244,18 +241,18 @@ def MagnetPosError_5d_jac(magnet_pos_5d, Bt, sensor_param, Measured_data):
     n_phi = math.sin(theta) * math.cos(phi)
     p_phi = 0
 
-    B_x = np.vstack((Blx_a*a_x, Bly_a*a_x, Blz_a*a_x)).transpose()
-    B_y = np.vstack((Blx_b*b_y, Bly_b*b_y, Blz_b*b_y)).transpose()
-    B_z = np.vstack((Blx_c*c_z, Bly_c*c_z, Blz_c*c_z)).transpose()
-    B_theta = np.vstack((
+    B_x = Bt*np.vstack((Blx_a*a_x, Bly_a*a_x, Blz_a*a_x)).transpose()
+    B_y = Bt*np.vstack((Blx_b*b_y, Bly_b*b_y, Blz_b*b_y)).transpose()
+    B_z = Bt*np.vstack((Blx_c*c_z, Bly_c*c_z, Blz_c*c_z)).transpose()
+    B_theta = Bt*np.vstack((
         Blx_m*m_theta + Blx_n*n_theta + Blx_p*p_theta,
         Bly_m*m_theta + Bly_n*n_theta + Bly_p*p_theta,
-        Blz_m*m_theta + Bly_n*n_theta + Blz_p*p_theta
+        Blz_m*m_theta + Blz_n*n_theta + Blz_p*p_theta
     )).transpose()
-    B_phi = np.vstack((
+    B_phi = Bt*np.vstack((
         Blx_m*m_phi + Blx_n*n_phi + Blx_p*p_phi,
         Bly_m*m_phi + Bly_n*n_phi + Bly_p*p_phi,
-        Blz_m*m_phi + Bly_n*n_phi + Blz_p*p_phi
+        Blz_m*m_phi + Blz_n*n_phi + Blz_p*p_phi
     )).transpose()
 
     Error_x = sum(sum(2 * Error_matrix * B_x))
@@ -264,10 +261,243 @@ def MagnetPosError_5d_jac(magnet_pos_5d, Bt, sensor_param, Measured_data):
     Error_theta = sum(sum(2 * Error_matrix * B_theta))
     Error_phi = sum(sum(2 * Error_matrix * B_phi))
 
-    return np.array([Error_x, Error_y, Error_z, Error_theta, Error_phi])
+    return np.array([0., 0., 0., Error_theta, Error_phi])
 
 
+def MagnetPosError_6d(magnet_pos_6d, Bt, sensor_param, Measured_data):
+
+    """
+    The function provide the error of one magnet_pos
+    :param magnet_pos: 1*6 magnet position and orientation vector (x, y, z, m, n, p)
+    :param Bt:
+    :param sensor_param: 16*6 sensor position and orientation matrix
+    :param Measured_data: 1*48 data
+    :return:
+    """
+    Measured_data = Measured_data.reshape((16,3))
+
+    sensor_pos = sensor_param[:, 0:3]
+    sensor_rotation = sensor_param[:, 3:6]
+    sensor_rotation_matrix = []
+    for r_v in sensor_rotation:
+        sensor_rotation_matrix.append( np.dot( yaw_matrix(r_v[2]), pitch_matrix(r_v[1]), roll_matrix(r_v[0]) ))
+    sensor_rotation_matrix = np.array(sensor_rotation_matrix)
+
+    magnet_pos = magnet_pos_6d
+
+    theo_data_init = M_field_value_model(magnet_pos, sensor_pos)
+    Theorectical_data = []
+    for i in range(theo_data_init.shape[0]):
+        Theorectical_data.append( np.dot(theo_data_init[i], sensor_rotation_matrix[i]).tolist() )
+    Theorectical_data = 1e7 * Bt * np.array(Theorectical_data)  #change to mG unit
+
+    E_Matrix = (Measured_data - Theorectical_data).transpose()
+    Error = sum(E_Matrix[0] ** 2) + sum(E_Matrix[1] ** 2) + sum(E_Matrix[2] ** 2)
+
+    return Error
 
 
+def MagnetPosError_6d_jac(magnet_pos_6d, Bt, sensor_param, Measured_data):
+
+    '''Theoretical magnetic value'''
+    Measured_data = Measured_data.reshape((16, 3))
+
+    sensor_pos = sensor_param[:, 0:3]
+    sensor_rotation = sensor_param[:, 3:6]
+    sensor_rotation_matrix = []
+    for r_v in sensor_rotation:
+        sensor_rotation_matrix.append(np.dot(yaw_matrix(r_v[2]), pitch_matrix(r_v[1]), roll_matrix(r_v[0])))
+    sensor_rotation_matrix = np.array(sensor_rotation_matrix)
+
+    magnet_pos = magnet_pos_6d
+
+    theo_data_init = M_field_value_model(magnet_pos, sensor_pos)
+    Theorectical_data = []
+    for i in range(theo_data_init.shape[0]):
+        Theorectical_data.append(np.dot(theo_data_init[i], sensor_rotation_matrix[i]).tolist())
+    Theorectical_data = 1e7 * Bt * np.array(Theorectical_data)  # change to mG unit
+
+    Error_matrix = Theorectical_data - Measured_data
+
+    '''derivative part'''
+    a = sensor_pos[:, 0] - magnet_pos[0]
+    b = sensor_pos[:, 1] - magnet_pos[1]
+    c = sensor_pos[:, 2] - magnet_pos[2]
+
+    m, n, p = magnet_pos[3], magnet_pos[4], magnet_pos[5]
+
+    Blx_a = (9 * m * a + 3 * n * b + 3 * p * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * a * (
+            3 * m * a ** 2 + 3 * n * b * c + 3 * p * c * a) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Bly_a = (3 * m * b + 3 * n * a) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * a * (
+            3 * m * a * b + 3 * n * b ** 2 + 3 * p * c * b) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Blz_a = (3 * m * c + 3 * p * a) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * a * (
+            3 * m * a * c + 3 * n * b * c + 3 * p * c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+
+    Blx_b = (3 * m * a + 3 * m * b) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * b * (
+            3 * m * a ** 2 + 3 * n * b * c + 3 * p * a * c) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Bly_b = (3 * m * a + 9 * n * b + 3 * p * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * b * (
+            3 * m * a * b + 3 * n * b ** 2 + 3 * p * c * b) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Blz_b = (3 * n * c + 3 * p * b) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * b * (
+            3 * m * a * c + 3 * n * b * c + 3 * p * c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+
+    Blx_c = (3 * p * a + 3 * m * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * c * (
+            3 * m * a ** 2 + 3 * n * b * a + 3 * p * c * a) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Bly_c = (3 * p * b + 3 * m * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * c * (
+            3 * m * a * b + 3 * n * b ** 2 + 3 * p * c * b) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Blz_c = (3 * m * a + 3 * n * b + 9 * p * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * c * (
+            3 * m * a * c + 3 * n * b * c + 3 * p * c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+
+    Blx_m = (2 * a ** 2 - b ** 2 - c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Bly_m = (3 * a * b) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Blz_m = (3 * a * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+
+    Blx_n = (3 * a * b) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Bly_n = (2 * b ** 2 - a ** 2 - c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Blz_n = (3 * b * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+
+    Blx_p = (3 * a * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Bly_p = (3 * b * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Blz_p = (2 * c ** 2 - b ** 2 - a ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+
+    a_x = -1
+    b_y = -1
+    c_z = -1
+
+    B_x = Bt*np.vstack((Blx_a*a_x, Bly_a*a_x, Blz_a*a_x)).transpose()
+    B_y = Bt*np.vstack((Blx_b*b_y, Bly_b*b_y, Blz_b*b_y)).transpose()
+    B_z = Bt*np.vstack((Blx_c*c_z, Bly_c*c_z, Blz_c*c_z)).transpose()
+    B_m = Bt*np.vstack((Blx_m, Bly_m, Blz_m )).transpose()
+    B_n = Bt*np.vstack((Blx_n, Bly_n, Blz_n)).transpose()
+    B_p = Bt*np.vstack((Blx_p, Bly_p, Blz_p)).transpose()
+
+    Error_x = sum(sum(2 * Error_matrix * B_x))
+    Error_y = sum(sum(2 * Error_matrix * B_y))
+    Error_z = sum(sum(2 * Error_matrix * B_z))
+    Error_m = sum(sum(2 * Error_matrix * B_m))
+    Error_n = sum(sum(2 * Error_matrix * B_n))
+    Error_p = sum(sum(2 * Error_matrix * B_p))
+
+    return np.array([Error_x, Error_y, Error_z, Error_m, Error_n, Error_p])
 
 
+def MagnetPosError_6d_jac_orientation(magnet_pos_6d, Bt, sensor_param, Measured_data):
+
+    '''Theoretical magnetic value'''
+    Measured_data = Measured_data.reshape((16, 3))
+
+    sensor_pos = sensor_param[:, 0:3]
+    sensor_rotation = sensor_param[:, 3:6]
+    sensor_rotation_matrix = []
+    for r_v in sensor_rotation:
+        sensor_rotation_matrix.append(np.dot(yaw_matrix(r_v[2]), pitch_matrix(r_v[1]), roll_matrix(r_v[0])))
+    sensor_rotation_matrix = np.array(sensor_rotation_matrix)
+
+    magnet_pos = magnet_pos_6d
+
+    theo_data_init = M_field_value_model(magnet_pos, sensor_pos)
+    Theorectical_data = []
+    for i in range(theo_data_init.shape[0]):
+        Theorectical_data.append(np.dot(theo_data_init[i], sensor_rotation_matrix[i]).tolist())
+    Theorectical_data = 1e7 * Bt * np.array(Theorectical_data)  # change to mG unit
+
+    Error_matrix = Theorectical_data - Measured_data
+
+    '''derivative part'''
+    a = sensor_pos[:, 0] - magnet_pos[0]
+    b = sensor_pos[:, 1] - magnet_pos[1]
+    c = sensor_pos[:, 2] - magnet_pos[2]
+    m, n, p = magnet_pos[3], magnet_pos[4], magnet_pos[5]
+
+    Blx_m = (2 * a ** 2 - b ** 2 - c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Bly_m = (3 * a * b) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Blz_m = (3 * a * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+
+    Blx_n = (3 * a * b) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Bly_n = (2 * b ** 2 - a ** 2 - c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Blz_n = (3 * b * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+
+    Blx_p = (3 * a * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Bly_p = (3 * b * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+    Blz_p = (2 * c ** 2 - b ** 2 - a ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2)
+
+    B_m = Bt*np.vstack((Blx_m, Bly_m, Blz_m )).transpose()
+    B_n = Bt*np.vstack((Blx_n, Bly_n, Blz_n)).transpose()
+    B_p = Bt*np.vstack((Blx_p, Bly_p, Blz_p)).transpose()
+
+    Error_m = sum(sum(2 * Error_matrix * B_m))
+    Error_n = sum(sum(2 * Error_matrix * B_n))
+    Error_p = sum(sum(2 * Error_matrix * B_p))
+
+    return np.array([0., 0., 0., Error_m, Error_n, Error_p])
+
+
+def MagnetPosError_6d_jac_position(magnet_pos_6d, Bt, sensor_param, Measured_data):
+
+    '''Theoretical magnetic value'''
+    Measured_data = Measured_data.reshape((16, 3))
+
+    sensor_pos = sensor_param[:, 0:3]
+    sensor_rotation = sensor_param[:, 3:6]
+    sensor_rotation_matrix = []
+    for r_v in sensor_rotation:
+        sensor_rotation_matrix.append(np.dot(yaw_matrix(r_v[2]), pitch_matrix(r_v[1]), roll_matrix(r_v[0])))
+    sensor_rotation_matrix = np.array(sensor_rotation_matrix)
+
+    magnet_pos = magnet_pos_6d
+
+    theo_data_init = M_field_value_model(magnet_pos, sensor_pos)
+    Theorectical_data = []
+    for i in range(theo_data_init.shape[0]):
+        Theorectical_data.append(np.dot(theo_data_init[i], sensor_rotation_matrix[i]).tolist())
+    Theorectical_data = 1e7 * Bt * np.array(Theorectical_data)  # change to mG unit
+
+    Error_matrix = Theorectical_data - Measured_data
+
+    '''derivative part'''
+    a = sensor_pos[:, 0] - magnet_pos[0]
+    b = sensor_pos[:, 1] - magnet_pos[1]
+    c = sensor_pos[:, 2] - magnet_pos[2]
+    m, n, p = magnet_pos[3], magnet_pos[4], magnet_pos[5]
+
+    Blx_a = (9 * m * a + 3 * n * b + 3 * p * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * a * (
+            3 * m * a ** 2 + 3 * n * b * c + 3 * p * c * a) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Bly_a = (3 * m * b + 3 * n * a) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * a * (
+            3 * m * a * b + 3 * n * b ** 2 + 3 * p * c * b) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Blz_a = (3 * m * c + 3 * p * a) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * a * (
+            3 * m * a * c + 3 * n * b * c + 3 * p * c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+
+    Blx_b = (3 * m * a + 3 * m * b) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * b * (
+            3 * m * a ** 2 + 3 * n * b * c + 3 * p * a * c) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Bly_b = (3 * m * a + 9 * n * b + 3 * p * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * b * (
+            3 * m * a * b + 3 * n * b ** 2 + 3 * p * c * b) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Blz_b = (3 * n * c + 3 * p * b) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * b * (
+            3 * m * a * c + 3 * n * b * c + 3 * p * c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+
+    Blx_c = (3 * p * a + 3 * m * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * c * (
+            3 * m * a ** 2 + 3 * n * b * a + 3 * p * c * a) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Bly_c = (3 * p * b + 3 * m * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * c * (
+            3 * m * a * b + 3 * n * b ** 2 + 3 * p * c * b) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+    Blz_c = (3 * m * a + 3 * n * b + 9 * p * c) * (a ** 2 + b ** 2 + c ** 2) ** (-5 / 2) - 5 * c * (
+            3 * m * a * c + 3 * n * b * c + 3 * p * c ** 2) * (a ** 2 + b ** 2 + c ** 2) ** (-7 / 2)
+
+    a_x = -1
+    b_y = -1
+    c_z = -1
+
+    B_x = Bt*np.vstack((Blx_a*a_x, Bly_a*a_x, Blz_a*a_x)).transpose()
+    B_y = Bt*np.vstack((Blx_b*b_y, Bly_b*b_y, Blz_b*b_y)).transpose()
+    B_z = Bt*np.vstack((Blx_c*c_z, Bly_c*c_z, Blz_c*c_z)).transpose()
+
+    Error_x = sum(sum(2 * Error_matrix * B_x))
+    Error_y = sum(sum(2 * Error_matrix * B_y))
+    Error_z = sum(sum(2 * Error_matrix * B_z))
+
+    return np.array([Error_x, Error_y, Error_z, 0., 0., 0.])
+
+
+def MagnetPosError_cons(magnet_pos_6d):
+    return magnet_pos_6d[3]**2 + magnet_pos_6d[4]**2 + magnet_pos_6d[5]**2
+
+
+def MagnetPosError_cons_jac(magnet_pos_6d):
+    return np.array([0., 0., 0., 2*magnet_pos_6d[3], 2*magnet_pos_6d[4], 2*magnet_pos_6d[5]])
